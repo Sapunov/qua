1afdedec3ca51f9f26b72d9f776e2cf7d50b4f98
dexig <dexig@mail.ru>
2017-01-25 22:21:46 +0300

    add version in footer

--- qua-ui/src/app/components/footer/footer.component.ts
+++ qua-ui/src/app/components/footer/footer.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { VERSION } from '../../../environments/const';
+
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.less']
 })
 export class FooterComponent implements OnInit {
+  version: string;
 
-  constructor() { }
+  constructor() {
+    this.version = VERSION;
+  }
 
   ngOnInit() {
   }
--- qua-ui/src/environments/const.ts
+++ qua-ui/src/environments/const.ts
@@ -6,3 +6,4 @@ export const URLS = {
 };
 
 export const MIN_CHARS_FOR_SEARCH = 2;
+export const VERSION = '1.0.0';
