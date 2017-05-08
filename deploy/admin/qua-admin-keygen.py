eaa656bb706610ba98990b9e62e0b05792678625
Nikita Sapunov <kiton1994@gmail.com>
2017-05-08 22:03:26 +0300

    New key generator dict

--- deploy/admin/qua-admin-keygen.py
+++ deploy/admin/qua-admin-keygen.py
@@ -7,8 +7,8 @@ import random
 chars = ''.join([
     string.ascii_letters,
     string.digits,
-    string.punctuation
-]).replace('\'', '').replace('"', '').replace('\\', '')
+    '!#$%&()*+,-.:;<=>?@[]^_`{|}~'
+])
 
 secret_key = ''.join(
     [random.SystemRandom().choice(chars) for i in range(50)]
